53e4f947387cbebc9e8de6abf486c6e63f8050a3
Sunghnkim95 <sunghnkim95@gmail.com>
2021-09-02 23:36:54 -0400

    add volleyball

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
+
+const app = require('express')()
+const volleyball = require('volleyball')
+
+app.use(volleyball)
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-const PORT = 1337;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
